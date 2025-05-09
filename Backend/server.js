1f28f11b7b65f9468bcd4935a97f6bfb2e5d08cc
Bansee <banceinasser36@gmail.com>
2025-05-10 02:29:49 +0300

    Added the route to get all products and filter based on  a type

--- Backend/server.js
+++ Backend/server.js
@@ -1,13 +1,33 @@
-const express = require('express')
 const dotenv = require('dotenv');
+const express = require('express')
 const connectDB   = require('./config/db.js')
-
+const Product =require ("./models/products.model.js")
 dotenv.config();
 
 const app=express();
+const PORT = process.env.PORT || 5000
+
+app.use(express.json()) // allows us to accept JSON data in the req.body
 
 
-app.listen(5000, () => {
-	//connectDB();
-	console.log("Server started at http://localhost: " + 5000);
+app.get("/api/home",async(req,res)=>{
+	try{
+		const {type} = req.query
+		const filter = {}
+		if(type){
+		 filter.type = {type: String(type) }
+		}
+
+		 const products = await Product.find(filter)
+		 res.status(200).json({success:true , data: products })
+	}catch(error){
+		console.log("Error fetching products:", error.message)
+		res.status(500).json({success:false,message:"Server Error"})
+	}
+
+})
+
+app.listen(PORT, () => {
+	connectDB();
+	console.log("Server started at http://localhost: " + PORT);
 });
